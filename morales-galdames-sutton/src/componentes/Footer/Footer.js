c786a4cc85c2213dbac3e75ca02e89c84cdd12f2
Iosef-Sutton <isuttondabbah@udesa.edu.ar>
2026-03-30 09:00:39 -0300

    Footer

--- morales-galdames-sutton/src/componentes/Footer/Footer.js
+++ morales-galdames-sutton/src/componentes/Footer/Footer.js
@@ -1,7 +1,12 @@
-import React from "react";
 
 function Footer(){
-    return(<></>)
+    return(
+        <>
+        <p>Paula Morales</p>
+        <p>Bernardita Galdames</p>
+        <p>Iosef Sutton</p>
+        </>
+    )
 }
 
 export default Footer

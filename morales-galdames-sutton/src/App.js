3e510cd5a1fccbe894598248247ec14c381238ac
Iosef-Sutton <isuttondabbah@udesa.edu.ar>
2026-03-30 08:44:59 -0300

    Intro.2

--- morales-galdames-sutton/src/App.js
+++ morales-galdames-sutton/src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 
 import { Switch, Route } from 'react-router-dom'
+import Header from "./componentes/Header/Header";
+import Footer from "./componentes/Footer/Footer";
 
 
 function App() {
-return(<></>)
+return(
+    <>
+    <Header/>
+    <Switch>
+        <Route path="/" exact={true} component={Home}/>
+        <Route path="/Favoritos" component={Favoritos}/>
+        <Route path="/Login" component={Login}/>
+        <Route path="/CrearCuenta" component={CrearCuenta}/>
+        <Route path="/VerTodas" component={VerTodas}/>
+    </Switch>
+    <Footer/>
+    </>
+)
 }
 
-export default App;
+export default App

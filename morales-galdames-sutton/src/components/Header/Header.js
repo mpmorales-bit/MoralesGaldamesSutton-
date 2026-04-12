788541b32f10597fee8aa6ca60a333f0fce98d54
Paula Morales <mpmorales@udesa.edu.ar>
2026-04-12 19:23:55 -0300

    Header

--- morales-galdames-sutton/src/components/Header/Header.js
+++ morales-galdames-sutton/src/components/Header/Header.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
-      <p>Header</p>
+      <h1>
+        <Link to="/">Movie App</Link>
+      </h1>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+
+          <li>
+            <Link to="/CrearCuenta">Crear Cuenta</Link>
+          </li>
+
+          <li>
+            <Link to="/Favoritos">Favoritos</Link>
+          </li>
+
+          <li>
+            <Link to="/Populares">Ver todas Populares</Link>
+          </li>
+
+          <li>
+            <Link to="/Toprated">Ver todas Top Rated</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }

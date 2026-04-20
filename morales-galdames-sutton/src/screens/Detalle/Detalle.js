7712b0b8f009e9bb4ad8c7e8df9afe92ca3ea236
Bernardita Galdames <bgaldames@udesa.edu.ar>
2026-04-20 14:10:10 -0300

    Arregle cookie de sesion detalle

--- morales-galdames-sutton/src/screens/Detalle/Detalle.js
+++ morales-galdames-sutton/src/screens/Detalle/Detalle.js
@@ -11,28 +11,46 @@ class Detalle extends Component{
         super(props);
         this.state = {
             informacion : null,
-            cargando : true
+            cargando : true,
+            esFavorito : false
         };
     }
+
     componentDidMount(){
         const id = this.props.match.params.id;
 
-        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0df8d28c1011b391dfb589da529c8b22`)
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=c20d2da133e58ff4cdd61efde80a09ce`)
             .then(response => response.json())
             .then(data => {
+                let yaEsFavorito = this.estaEnFavoritos(data.id);
+                
                 this.setState({
-                informacion: data,
-                cargando : false})
+                     informacion: data,
+                     cargando : false,
+                     esFavorito : yaEsFavorito
+                })
             })
-            .catch(error => console.log(error))
+            .catch(error => console.log(error))    
+    };
+
+    estaEnFavoritos(id) {
+        let favoritos = localStorage.getItem("favoritos");
+        favoritos = (favoritos === null) ? [] : JSON.parse(favoritos);
+
+        let encontrado = favoritos.filter(fav => fav.id === id);
+        return encontrado.length > 0;
     }
 
-    agregarFavorito(){
+    agregarAFavoritos=() => {
+        if (this.state.esFavorito) {
+            alert("Esta pelicula ya está en favoritos");
+            return;
+        }
+
         let favoritos = localStorage.getItem("favoritos");
-        (favoritos === null)? favoritos=[] : favoritos=JSON.parse(favoritos);
+        favoritos = (favoritos === null)? [] : JSON.parse(favoritos);
 
         let info = this.state.informacion;
-
         let favorito = {
             id : info.id,
             titulo : info.title,
@@ -42,26 +60,42 @@ class Detalle extends Component{
         
         favoritos.push(favorito);
         localStorage.setItem("favoritos", JSON.stringify(favoritos));
+    
+        this.setState({esFavorito:true})
+        alert("Lo agregaste a favoritos!");
+    };
+
+    eliminarDeFavoritos = () => {
+        let favoritos = JSON.parse(localStorage.getItem("favoritos"));
+        let favoritoNuevo = favoritos.filter(fav => fav.id !== this.state.informacion.id);
+
+        localStorage.setItem("favoritos", JSON.stringify(favoritoNuevo));
+        
+        this.setState({esFavorito : false});
+        alert("Eliminaste esta pelicula de favoritos");
     }
 
+
     render(){
-        if (this.state.informacion === null){
-            return <Loader />;
-        }
+        if (this.state.cargando) return <Loader />;
 
         let info = this.state.informacion;
 
         return(
             <>
                <Header />
-               <img src={`https://image.tmdb.org/t/p/w480/${info.poster_path}`} alt={info.title}/>
+               <img src={`https://image.tmdb.org/t/p/w500${info.poster_path}`} alt={info.title}/>
                <h1>{info.title}</h1>
                <p>Calificacion: {info.vote_average}</p>
                <p>Fecha de estreno: {info.release_date}</p>
                <p>Duración: {info.runtime} minutos</p>
                <p>Sinopsis: {info.overview}</p>
                <p>Genero:{info.genres ? info.genres.map(genero => genero.name): "No tiene genero"}</p>
-               {cookies.get("user-auth-cookie") !== "undefined" ? <button onClick={() => this.agregarFavorito()}>Agregar a favoritos</button> : null}
+               {cookies.get("user-auth-cookie") && (
+                    this.state.esFavorito ? (
+                        <button onClick={this.eliminarDeFavoritos}>♡ Quitar de favoritos</button>
+                    ) : (
+                        <button className="btn-fav alert-primary" onClick={() => this.agregarAFavoritos()}>♡</button>))}
                <Footer/>
             </>
         )
